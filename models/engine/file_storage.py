b21cc0cfc1efcd7262d818efabd227fd4aeb3b2e
Errafai <errafaiachraf1923@gmail.com>
2024-12-08 13:00:40 +0100

    file_storage delete all

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -9,7 +9,8 @@ class FileStorage:
     __objects = {}
 
     def all(self, cls=None):
-        """Returns a dictionary of models currently in storage"""
+        """Returns a dictionary of models currently in storage
+        and all cls only if not none"""
         if cls is None:
            return FileStorage.__objects
         cls_by_name = {}
